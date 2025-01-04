0aea901dd2b2f5b564ed6b345de91507b5d4abf3
Zakriahassan44 <kzakria576@gmail.com>
2025-01-04 11:14:12 -0800

    done

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+//What is variable & Data Type || Dart Programming Tutorials in Hindi/Urdu
+void main() {
+  // Declaring a variable
+  //void is a reserved keyword in Dart that is used to define a function that does not return any value.
+  //rules for declaring a variable
+  //small letter starting
+  //2nd word onwards first letter should be capital
+  //no special characters
+
+  //int is a reserved keyword in Dart that is used to define integer variables.
+  int numberOne;
+  int numberTwo;
+  int sum;
+  int subtraction;
+  int multiplication;
+  //we cannot store the result of division in an int variable because the result of division is a double.
+  // int division;
+  double division;
+
+  numberOne = 1;
+  numberTwo = 2;
+  //sum subtraction and multiplication .
+  sum = numberOne + numberTwo;
+  subtraction = numberTwo - numberOne;
+  multiplication = numberOne * numberTwo;
+  //fist they give me an error because the result of the division is a double
+  division = numberTwo / numberOne;
+
+  //printing the values
+  print(numberOne);
+  print(numberTwo);
+  print(sum);
+  print(subtraction);
+  print(multiplication);
+  print(division);
+}
