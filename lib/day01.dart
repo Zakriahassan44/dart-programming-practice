3b8c2a4fdb5c18e7ae621311075a5704e341cb46
Zakriahassan44 <kzakria576@gmail.com>
2025-01-05 00:36:05 -0800

    done

---
+++ lib/day01.dart
@@ -0,0 +1,12 @@
+void main() {
+  String age = 'hello';
+  String index = '01234';
+  String result;
+
+  result = age + index; // this is how we concatenate strings
+// this is how we get the length of a string
+  print(age.length);
+  print(index.length);
+  print(age[0]); // this is how we get the first character of a string
+  print(result);
+}
---
+++ lib/day02.dart
@@ -0,0 +1,32 @@
+void main() {
+  // int x = 20;
+  var data;
+  // data = {"name": "John", "age": 30};
+
+  data = {
+    'map': 1,
+    'data': 2,
+  };
+
+  print(data['map']);
+  // print(data[0]);
+//var is rally powefull data type
+}
+
+
+
+
+
+
+
+
+
+
+// void main() {
+//   int numberOne = 1;
+//   int numberTwo = 2;
+//   bool result = true;
+//   print(numberOne > numberTwo);
+// }
+
+
